eecdfaf379177a9772cff32f902ef7b260e6f254
msenior526 <msenior526@gmail.com>
2021-03-08 14:09:04 -0500

    commit tree 34d25cdafc3f35eefb324b16c5466b052b0cbc27

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, presence: true, length: {minimum: 250}
+    validates :category, presence: true, inclusion: {in: %w(Fiction Non-Fiction)}
 end
